5c7ecb97d4111831bcf8e010efc0a17ffbdebedc
Tanmay <tanmay.milky33@gmail.com>
2021-02-02 20:13:54 +0530

    Update settings.py

--- uniacco_rest/settings.py
+++ uniacco_rest/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'django.contrib.sites',
 ]
-SITE_ID = 2
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
