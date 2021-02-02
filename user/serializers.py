783d200e2b4517a02830a72eea89c034524ef326
Tanmay <tanmay.milky33@gmail.com>
2021-02-02 19:16:42 +0530

    Update serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -33,7 +33,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password']
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5,'style':{'input_type': 'password'}}}
 
     def create(self, validated_data):
         """Create a new user with encrypted pass"""
